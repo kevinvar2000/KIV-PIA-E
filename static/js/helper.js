c0e6986ab1a61d93196570a8785b39db29f86786
Kevin Varchola <kevin.keko321@gmail.com>
2025-10-02 13:40:53 +0200

    Fixed and added static files

---
+++ static/js/helper.js
@@ -0,0 +1,16 @@
+function toggleLanguages() {
+    var role = document.getElementById('id_role').value;
+    var langDiv = document.getElementById('languages-container');
+    langDiv.style.display = (role === 'translator') ? 'block' : 'none';
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    $('#id_languages').selectpicker();
+
+    // Optional: show selected languages below
+    $('#id_languages').on('changed.bs.select', function () {
+    let selected = $(this).find("option:selected").map(function(){ return $(this).text(); }).get();
+    document.getElementById("selected-languages-text").textContent = 
+        selected.length ? "Selected: " + selected.join(", ") : "";
+    });
+});
